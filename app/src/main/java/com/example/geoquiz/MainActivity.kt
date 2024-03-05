8c7de66b6c7dc33ea94624ba878e819ea5349980
Maksim Davletkulov <m160598@gmail.com>
2024-03-05 10:07:01 +0300

    Chapter 3 finished

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.geoquiz
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.geoquiz.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,10 +22,39 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_asia, true)
     )
 
+    private val answers = questionBank.map { it.textResId }.associateBy({ it }, { false })
+        .toMutableMap()
+
     private var currentIndex = 0
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -38,26 +68,66 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
-            updateQuestion()
+            nextQuestion()
         }
 
-        updateQuestion()
+        binding.previousButton.setOnClickListener {
+            previousQuestion()
+        }
+
+        binding.questionTextView.setOnClickListener {
+            nextQuestion()
+        }
+
+        updateQuizViews()
     }
 
-    private fun updateQuestion() {
+    private fun nextQuestion() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+        updateQuizViews()
+    }
+
+    private fun previousQuestion() {
+        currentIndex = (questionBank.size + currentIndex - 1) % questionBank.size
+        updateQuizViews()
+    }
+
+    private fun updateQuizViews() {
         val questionTextResId = questionBank[currentIndex].textResId
         binding.questionTextView.setText(questionTextResId)
+        when (currentIndex) {
+            0 -> {
+                binding.nextButton.isEnabled = true
+                binding.previousButton.isEnabled = false
+            }
+
+            questionBank.lastIndex -> {
+                binding.nextButton.isEnabled = false
+                binding.previousButton.isEnabled = true
+            }
+
+            else -> {
+                binding.nextButton.isEnabled = true
+                binding.previousButton.isEnabled = true
+            }
+        }
+
     }
 
     private fun checkAnswer(userAnswer: Boolean, view: View) {
-        val currentQuestionAnswer = questionBank[currentIndex].answer
+        val question = questionBank[currentIndex]
+        val currentQuestionAnswer = question.answer
         val messageResId = if (currentQuestionAnswer == userAnswer) {
+            answers[question.textResId] = true
             R.string.correct_toast
         } else {
+            answers[question.textResId] = false
             R.string.incorrect_toast
         }
-
-        Snackbar.make(view, messageResId, Snackbar.LENGTH_SHORT).show()
+        if (answers.all { it.value }) {
+            Snackbar.make(view, R.string.quiz_success, Snackbar.LENGTH_SHORT).show()
+        } else {
+            Snackbar.make(view, messageResId, Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
